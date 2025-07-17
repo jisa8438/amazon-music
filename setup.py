670d0842c80e125b93de8fea1ce4c562a38abba0
DAMIEN <aminesoukara@gmail.com>
2025-07-17 03:42:53 +0100

    push

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import find_packages, setup
 AUTHOR = "Amine Soukara"
 EMAIL = "AmineSoukara@gmail.com"
 URL = "https://github.com/AmineSoukara/Amazon-Music"
-VERSION = "1.7.4"
+VERSION = "1.7.5"
 
 # Get the long description from README
 
